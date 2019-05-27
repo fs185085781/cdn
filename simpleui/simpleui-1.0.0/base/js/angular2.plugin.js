f39024605d061c8dd0b3dcd9d370491265b6f4a3
fs185085781 <185085781@qq.com>
2019-05-27 18:20:49 +0800

    1

--- simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
+++ simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
@@ -1,3 +1,17 @@
 (function(win){
     "use strict";
+    var ui = win.simple;
+    win.SimpleUiComponent = ng.core.Component({
+        selector: '[simpleUi]',
+        template: '<div style="width:200px;height:200px;"></div>',
+        providers: [ng.core.ElementRef],
+    }).Class({
+        constructor: [ng.core.ElementRef, function(ref) {
+            this.ref.nativeElement.style.backgroundColor = "yellow";
+            this.data = pageData;
+        }],
+        ngAfterViewChecked:function(){
+            console.log("组件",this);
+        }
+    });
 })(window)
