d9119a4a282bfaa5bb7a5aebe9ce5502fc5e9a44
fs <185085781@qq.com>
2019-05-28 07:24:34 +0800

    1

--- simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
+++ simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
@@ -1,19 +1,103 @@
 (function(win){
     "use strict";
     var ui = win.simple;
-    win.SimpleUiComponent = ng.core.Component({
-        selector: '[simpleUi]',
-        inputs: ['simpleUi'],
-        template: '<div style="width:200px;height:200px;">嘻嘻</div>',
-        providers: [ng.core.ElementRef]
-    }).Class({
-        constructor: [ng.core.ElementRef, function(ref) {
-            console.log(ref);
-            //this.ref.nativeElement.style.backgroundColor = "yellow";
-            this.data = pageData;
-        }],
-        ngAfterViewChecked:function(){
-            console.log("组件",this);
+    win.SimpleUiComponent = function(clazz){
+        if(!ui.angular2ModuleMap){
+            ui.angular2ModuleMap = {};
         }
-    });
+        var module = ui.moduleMap[clazz];
+        if(!module){
+            console.log("此模块不存在");
+        }
+        var oneComponent= ui.angular2ModuleMap[clazz];
+        if(oneComponent){
+            return oneComponent;
+        }
+        var selector ='['+clazz+']';
+        var moduleObj = new module();
+        var eventMap = moduleObj.eventMap;
+        var fieldMap = moduleObj.fieldMap;
+        var template = "<div";
+        template +=" class=\""+clazz+"\"";
+        for(var field in fieldMap){
+            template +=" [attr."+field+"]=\"simpleUiData."+field+"\"";
+        }
+        /*for(var event in eventMap){
+            template +=" [attr.el"+event+"]=\"simpleUiData.el"+event+"\"";
+        }*/
+        template +="></div>";
+        var oneComponent= ng.core.Component({
+            selector: selector,
+            inputs: ['simpleUiData'],
+            template: template,
+            providers: [ng.core.ElementRef]
+        }).Class({
+            constructor: [ng.core.ElementRef, function(ref) {
+                this.el = ref.nativeElement;
+            }],
+            ngAfterViewChecked:function(){
+                var uiEl = iBase(this.el).find("."+clazz);
+                this.uiEl = uiEl;
+                ui.parse(uiEl);
+                var uiObj = ui.getBySelect(uiEl);
+                if(!uiObj){
+                    return;
+                }
+                this.ui = uiObj;
+                uiObj.angular2 = this;
+                var allBindMap = ui.angular2ModuleMap[clazz].allBindMap;
+                if(allBindMap){
+                    for(var event in eventMap){
+                        var val = this.simpleUiData["el"+event];
+                        if(!val){
+                            continue;
+                        }
+                        var func = allBindMap[val];
+                        if(!func){
+                            continue;
+                        }
+                        var hasBindMap = uiObj.allBindEventMap;
+                        if(!hasBindMap){
+                            hasBindMap = {};
+                        }
+                        if(hasBindMap[event]){
+                            continue;
+                        }
+                        uiObj.on(event,func);
+                    }
+                }
+                //处理数据变化
+                if(!this.oldDataMap){
+                    this.oldDataMap = {};
+                }
+                var oldDataMap = this.oldDataMap;
+                for(var field in fieldMap){
+                    var val = this.simpleUiData[field];
+                    var oldVal = oldDataMap[field];
+                    if(oldVal == val){
+                        continue;
+                    }
+                    var setFunctionName = "set"+ui.firstToUpperCase(field);
+                    if(uiObj[setFunctionName]){
+                        eval("uiObj."+setFunctionName+"(val)");
+                    }else{
+                        uiObj[field] = val;
+                    }
+                    oldDataMap[field] = val;
+                }
+            }
+        });
+        ui.angular2ModuleMap[clazz] = oneComponent;
+        oneComponent.clazz = clazz;
+        return oneComponent;
+    }
+    win.SimpleUiBindEvent=function(module,methodName,func){
+        var clazz = module.clazz;
+        var allBindMap = ui.angular2ModuleMap[clazz].allBindMap;
+        if(!allBindMap){
+            allBindMap = {};
+            ui.angular2ModuleMap[clazz].allBindMap = allBindMap;
+        }
+        allBindMap[methodName] = func;
+    };
 })(window)
