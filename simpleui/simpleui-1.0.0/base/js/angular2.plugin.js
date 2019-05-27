e1c12faeba2aff40b9b0cdcbde40981d7b71064c
fs <185085781@qq.com>
2019-05-28 02:57:44 +0800

    1

--- simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
+++ simpleui/simpleui-1.0.0/base/js/angular2.plugin.js
@@ -3,11 +3,13 @@
     var ui = win.simple;
     win.SimpleUiComponent = ng.core.Component({
         selector: '[simpleUi]',
-        template: '<div style="width:200px;height:200px;"></div>',
-        providers: [ng.core.ElementRef],
+        inputs: ['simpleUi'],
+        template: '<div style="width:200px;height:200px;">嘻嘻</div>',
+        providers: [ng.core.ElementRef]
     }).Class({
         constructor: [ng.core.ElementRef, function(ref) {
-            this.ref.nativeElement.style.backgroundColor = "yellow";
+            console.log(ref);
+            //this.ref.nativeElement.style.backgroundColor = "yellow";
             this.data = pageData;
         }],
         ngAfterViewChecked:function(){
