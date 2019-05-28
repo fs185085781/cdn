9bfb7d0ef41863c915d008de8fa7018f3b6268a8
fs185085781 <185085781@qq.com>
2019-05-28 18:23:29 +0800

    1

--- simpleui/simpleui-1.0.0/base/js/angular.plugin.js
+++ simpleui/simpleui-1.0.0/base/js/angular.plugin.js
@@ -1,17 +1,19 @@
 (function(win){
     "use strict";
-    alert("实在抱歉,暂时还不能支持angular");
+   //alert("实在抱歉,暂时还不能支持angular");
    var module = angular.module;
     angular.module = function(name, requires, configFn){
         var app = module(name, requires, configFn);
-        var controller = app.controller;
-        app.controller = function(recipeName, factoryFunction){
-            var tempFunction = factoryFunction;
-            factoryFunction = function($scope){
-                return tempFunction($scope);
+        app.directive('simpleUi', function($document) {
+            return function(scope, parent, attr) {
+                console.log($document);
+                var html = '<div name="{{buttons[0].name}}" text="{{buttons[0].text}}"></div>';
+                var template = angular.element(html);
+                var linkFn = $compile(template);
+                var element = linkFn(scope);
+                parent.appendChild(element);
             }
-            return controller(recipeName,factoryFunction);
-        }
+        });
         return app;
     }
 })(window)
