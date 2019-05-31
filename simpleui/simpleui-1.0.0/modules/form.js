248a817fb5f3fb6a179368810fd03b8cb1c85382
fs <185085781@qq.com>
2019-06-01 00:51:49 +0800

    1

--- simpleui/simpleui-1.0.0/modules/form.js
+++ simpleui/simpleui-1.0.0/modules/form.js
@@ -509,5 +509,31 @@
         thisClass:buttongroup,
         init:buttongroup.init
     });
+    /*输入框*/
+    var textbox = {
+        init:function(){
+
+        },
+        validate:function(){
+
+        },
+        isValid:function(){
+
+        },
+        setIsValid:function(){
+
+        }
+
+    };
+    ui.TextBox = function(){};
+    ui.regModule({
+        clazz:ui.TextBox,
+        useClass:ui.prefix+"-textbox",
+        fields:["emptyText","value","allowInput","selectOnFocus","maxLength","errorMode","validateOnChanged","validateOnLeave","forceValidate","required","requiredErrorText","vtype","vtypeErrorText"],
+        events:["valuechanged","validation","enter","keydown","keyup","focus","blur"],
+        parentClass:ui.BaseModule,
+        thisClass:textbox,
+        init:textbox.init
+    });
 
 })(window);
