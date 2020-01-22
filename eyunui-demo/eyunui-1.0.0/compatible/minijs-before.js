ab760b19923a24265e591d31835f23d085164c63
fansheng <185085781@qq.com>
2020-01-22 14:26:49 +0800

    1

--- eyunui-demo/eyunui-1.0.0/compatible/minijs-before.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    utils.evalData = window.eval;
-    window.eval = function(x){
-        x = x.replace("location=\"http://www.miniui.com\"",";");
-        x = x.replace("alert(\"试用到期 www.miniui.com\")",";");
-        x = x.replace("location = \"http://www.miniui.com\"",";");
-        return utils.evalData(x);
-    }
-})()
