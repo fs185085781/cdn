83e8257fc1efe8e58215efc21f028b9828eac4b0
fansheng <185085781@qq.com>
2020-02-01 18:49:43 +0800

    1

--- minit-ui-demo/minit-ui-1.0.0/plugins/utils-expand/utils-expand.js
+++ minit-ui-demo/minit-ui-1.0.0/plugins/utils-expand/utils-expand.js
@@ -1,30 +1,156 @@
 (function (tools) {
     tools.$ = {
+        vue:new Vue({}),
         successMsg:function(text){
             this.msg(text,"success");
         },
+        infoMsg:function(text){
+            this.msg(text,"info");
+        },
+        warningMsg:function(text){
+            this.msg(text,"warning");
+        },
+        dangerMsg:function(text){
+            this.msg(text,"danger");
+        },
         msg:function(text,type){
+            var that = this;
+            if(!type){
+                type = "default";
+            }
+            if(tools.from != "m" && tools.from != "pc"){
+                return;
+            }
+            if(tools.from == "m"){
+                that.vue.$toast({
+                    message:text,
+                    duration:3000,
+                    className:type,
+                    position:"bottom"
+                });
+            }else{
+                mini.showTips({
+                    content: text,
+                    state: type,
+                    x: "center",
+                    y: "top",
+                    timeout: 3000
+                });
+            }
+        },
+        loading:function(text){
+            if(!text){
+                text = "";
+            }
             var that = this;
             if(tools.from != "m" && tools.from != "pc"){
                 return;
             }
             if(tools.from == "m"){
-                msgM(text,type);
+                that.vue.$indicator.open({
+                    text:text
+                });
             }else{
-                msgPc(text,type);
-            }
-            function msgM(text,type){
-                if(!that.vue){
-                    that.vue = new Vue();
+                if(that.loadingId){
+                    mini.hideMessageBox(that.loadingId);
+                    tools.removeProp(that,"loadingId");
                 }
-                that.vue.$toast({
-                    message:text,
-                    duration:-1,
-                    className:type
+                that.loadingId = mini.loading(text);
+            }
+        },
+        cancelLoading:function(){
+            var that = this;
+            if(tools.from != "m" && tools.from != "pc"){
+                return;
+            }
+            if(tools.from == "m"){
+                that.vue.$indicator.close();
+            }else{
+                if(that.loadingId){
+                    mini.hideMessageBox(that.loadingId);
+                    tools.removeProp(that,"loadingId");
+                }
+            }
+        },
+        alert:function(text,callback){
+            var that = this;
+            if(tools.from != "m" && tools.from != "pc"){
+                return;
+            }
+            if(tools.from == "m"){
+                that.vue.$messagebox.alert(text).then(function(){
+                    if(callback){
+                        callback(1);
+                    }
+                });
+            }else{
+                mini.alert(text, "提示", function(action){
+                    if(callback){
+                        if(action == "ok"){
+                            callback(1);
+                        }else{
+                            callback(-1);
+                        }
+                    }
                 });
             }
-            function msgPc(text,type){
-
+        },
+        confirm:function(text,callback){
+            var that = this;
+            if(tools.from != "m" && tools.from != "pc"){
+                return;
+            }
+            if(tools.from == "m"){
+                that.vue.$messagebox.confirm(text).then(function(){
+                    if(callback){
+                        callback(1);
+                    }
+                }).catch(function () {
+                    if(callback){
+                        callback(0);
+                    }
+                });
+            }else{
+                mini.confirm(text, "提示", function(action){
+                    if(callback){
+                        if(action == "ok"){
+                            callback(1);
+                        }else if(action == "cancel"){
+                            callback(0);
+                        }else{
+                            callback(-1);
+                        }
+                    }
+                });
+            }
+        },
+        prompt:function(text,callback){
+            var that = this;
+            if(tools.from != "m" && tools.from != "pc"){
+                return;
+            }
+            if(tools.from == "m"){
+                that.vue.$messagebox.prompt(text).then(function(data){
+                    if(callback){
+                        callback(1,data.value);
+                    }
+                }).catch(function () {
+                    if(callback){
+                        callback(0);
+                    }
+                });
+            }else{
+                mini.prompt(text, "提示", function(action,val){
+                    if(callback){
+                        if(action == "ok"){
+                            callback(1,val);
+                        }else if(action == "cancel"){
+                            callback(0);
+                        }else{
+                            callback(-1);
+                        }
+                    }
+                });
             }
         }
     }
