32105d003c44a38a057f767fa91258d30227a1af
fansheng <185085781@qq.com>
2020-02-09 20:52:43 +0800

    1

--- eamv-demo/eamv-ui-1.0.0/boot/config.js
+++ eamv-demo/eamv-ui-1.0.0/boot/config.js
@@ -3,5 +3,12 @@
         env:"prod",
         configType:"local"
     }
+    window.reqHook = function(data){
+        var res = {flag:false,msg:"请修改config.js的reqHook方法",data:data};
+        return res;
+    }
+    window.uploadUrlHook = function(){
+        return "请修改config.js的uploadUrlHook方法";
+    }
     window.config = c;
 })()
