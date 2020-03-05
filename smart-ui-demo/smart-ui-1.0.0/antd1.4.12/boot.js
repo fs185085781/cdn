bb317fdc45be912399c0a4cbe35889230ad0c944
fansheng <185085781@qq.com>
2020-03-05 10:26:40 +0800

    1

--- smart-ui-demo/smart-ui-1.0.0/antd1.4.12/boot.js
+++ smart-ui-demo/smart-ui-1.0.0/antd1.4.12/boot.js
@@ -1,7 +1,7 @@
 (function(){
     var path = getJsPath("boot.js",1);
     document.write('<script src="' + path + '/vue.min.js" type="text/javascript"></sc' + 'ript>');
-    document.write('<script src="' + path + '/antd.js" type="text/javascript" ></sc' + 'ript>');
+    document.write('<script src="' + path + '/antd.min.js" type="text/javascript" ></sc' + 'ript>');
     document.write('<link href="' + path + '/antd.min.css" rel="stylesheet" type="text/css" />');
     document.write('<script src="' + path + '/dialog-drag.js" type="text/javascript" ></sc' + 'ript>');
     document.write('<link href="' + path + '/dialog-drag.css" rel="stylesheet" type="text/css" />');
