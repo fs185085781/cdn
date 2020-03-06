63bd8ece2a37137a74091f33ada1feeb9a129be0
fansheng <185085781@qq.com>
2020-03-06 12:06:49 +0800

    1

--- smart-ui-1.0.0/miniui3.9.2/boot.js
+++ smart-ui-1.0.0/miniui3.9.2/boot.js
@@ -6,6 +6,7 @@
     document.write('<script src="' + path + '/miniui/minijs-after.js" type="text/javascript" ></sc' + 'ript>');
     document.write('<script src="' + path + '/miniui/export-execl.js" type="text/javascript" ></sc' + 'ript>');
     document.write('<link href="' + path + '/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />');
+    document.write('<link href="' + path + '/font-awesome/css/fonts.css" rel="stylesheet" type="text/css" />');
     document.write('<link href="' + path + '/miniui/themes/default/miniui.css" rel="stylesheet" type="text/css" />');
     document.write('<link href="' + path + '/miniui/themes/metro-green/skin.css" rel="stylesheet" type="text/css" />');
     document.write('<link href="' + path + '/miniui/themes/default/medium-mode.css" rel="stylesheet" type="text/css" />');
