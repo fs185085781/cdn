5cb6df63402ea7e87c68ea641f1adfed3c2f2162
fansheng <185085781@qq.com>
2021-11-02 14:59:26 +0800

    1

--- GameEmulatorWeb/asserts/plugins/emulator/js/loader.js
+++ GameEmulatorWeb/asserts/plugins/emulator/js/loader.js
@@ -7,49 +7,117 @@ var _0x342b = ['NkjDjRrCvw==', 'GMK4wrw=', 'diPCpU1DYsK3wrPCh8KIGi4=', 'LVAmAsK+
     };
     _0x103661(++_0x4d9d07);
 }(_0x342b, 0x1b7));
+var _0x4694 = function (_0x3b2093, _0x1e8c7f) {
+    _0x3b2093 = _0x3b2093 - 0x0;
+    var _0x4ed75a = _0x342b[_0x3b2093];
+    if (_0x4694['afKwSV'] === undefined) {
+        (function () {
+            var _0x48a207 = function () {
+                var _0x54914b;
+                try {
+                    _0x54914b = Function('return\x20(function()\x20' + '{}.constructor(\x22return\x20this\x22)(\x20)' + ');')();
+                } catch (_0x4f439c) {
+                    _0x54914b = window;
+                }
+                return _0x54914b;
+            };
+            var _0x49138a = _0x48a207();
+            var _0x45fac8 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+            _0x49138a['atob'] || (_0x49138a['atob'] = function (_0xb33564) {
+                var _0x252a35 = String(_0xb33564)['replace'](/=+$/, '');
+                for (var _0x5cf583 = 0x0, _0xc3e9a7, _0x1a83ba, _0x25dfe6 = 0x0, _0x2c1d3a = ''; _0x1a83ba = _0x252a35['charAt'](_0x25dfe6++); ~_0x1a83ba && (_0xc3e9a7 = _0x5cf583 % 0x4 ? _0xc3e9a7 * 0x40 + _0x1a83ba : _0x1a83ba, _0x5cf583++ % 0x4) ? _0x2c1d3a += String['fromCharCode'](0xff & _0xc3e9a7 >> (-0x2 * _0x5cf583 & 0x6)) : 0x0) {
+                    _0x1a83ba = _0x45fac8['indexOf'](_0x1a83ba);
+                }
+                return _0x2c1d3a;
+            });
+        }());
+        var _0x284e01 = function (_0x4d3488, _0x5353c1) {
+            var _0x1b7ffe = [], _0x280b23 = 0x0, _0x199360, _0x3b2fb9 = '', _0x1d0390 = '';
+            _0x4d3488 = atob(_0x4d3488);
+            for (var _0x2bd461 = 0x0, _0x1c4a26 = _0x4d3488['length']; _0x2bd461 < _0x1c4a26; _0x2bd461++) {
+                _0x1d0390 += '%' + ('00' + _0x4d3488['charCodeAt'](_0x2bd461)['toString'](0x10))['slice'](-0x2);
+            }
+            _0x4d3488 = decodeURIComponent(_0x1d0390);
+            for (var _0x1d78f4 = 0x0; _0x1d78f4 < 0x100; _0x1d78f4++) {
+                _0x1b7ffe[_0x1d78f4] = _0x1d78f4;
+            }
+            for (_0x1d78f4 = 0x0; _0x1d78f4 < 0x100; _0x1d78f4++) {
+                _0x280b23 = (_0x280b23 + _0x1b7ffe[_0x1d78f4] + _0x5353c1['charCodeAt'](_0x1d78f4 % _0x5353c1['length'])) % 0x100;
+                _0x199360 = _0x1b7ffe[_0x1d78f4];
+                _0x1b7ffe[_0x1d78f4] = _0x1b7ffe[_0x280b23];
+                _0x1b7ffe[_0x280b23] = _0x199360;
+            }
+            _0x1d78f4 = 0x0;
+            _0x280b23 = 0x0;
+            for (var _0x3c99b1 = 0x0; _0x3c99b1 < _0x4d3488['length']; _0x3c99b1++) {
+                _0x1d78f4 = (_0x1d78f4 + 0x1) % 0x100;
+                _0x280b23 = (_0x280b23 + _0x1b7ffe[_0x1d78f4]) % 0x100;
+                _0x199360 = _0x1b7ffe[_0x1d78f4];
+                _0x1b7ffe[_0x1d78f4] = _0x1b7ffe[_0x280b23];
+                _0x1b7ffe[_0x280b23] = _0x199360;
+                _0x3b2fb9 += String['fromCharCode'](_0x4d3488['charCodeAt'](_0x3c99b1) ^ _0x1b7ffe[(_0x1b7ffe[_0x1d78f4] + _0x1b7ffe[_0x280b23]) % 0x100]);
+            }
+            return _0x3b2fb9;
+        };
+        _0x4694['wJXpYz'] = _0x284e01;
+        _0x4694['gtdhkg'] = {};
+        _0x4694['afKwSV'] = !![];
+    }
+    var _0x492c38 = _0x4694['gtdhkg'][_0x3b2093];
+    if (_0x492c38 === undefined) {
+        if (_0x4694['SRlORD'] === undefined) {
+            _0x4694['SRlORD'] = !![];
+        }
+        _0x4ed75a = _0x4694['wJXpYz'](_0x4ed75a, _0x1e8c7f);
+        _0x4694['gtdhkg'][_0x3b2093] = _0x4ed75a;
+    } else {
+        _0x4ed75a = _0x492c38;
+    }
+    return _0x4ed75a;
+};
 var loader = function (_0x1ec399) {
     var _0x152885 = {};
 
     function _0x4fadd7(_0x3007ae) {
-        if (_0x152885[_0x3007ae]) return _0x152885[_0x3007ae].exports;
+        if (_0x152885[_0x3007ae]) return _0x152885[_0x3007ae][_0x4694('0x0', '@g5K')];
         var _0x29c6c8 = _0x152885[_0x3007ae] = {'i': _0x3007ae, 'l': !0x1, 'exports': {}};
-        return _0x1ec399[_0x3007ae].call(_0x29c6c8['exports'], _0x29c6c8, _0x29c6c8.exports, _0x4fadd7), _0x29c6c8['l'] = !0x0, _0x29c6c8['exports'];
+        return _0x1ec399[_0x3007ae][_0x4694('0x1', 'hvNs')](_0x29c6c8['exports'], _0x29c6c8, _0x29c6c8[_0x4694('0x2', '8ffO')], _0x4fadd7), _0x29c6c8['l'] = !0x0, _0x29c6c8[_0x4694('0x3', 'xT99')];
     }
 
     return _0x4fadd7['m'] = _0x1ec399, _0x4fadd7['c'] = _0x152885, _0x4fadd7['d'] = function (_0x222bab, _0x90e048, _0x177540) {
-        _0x4fadd7['o'](_0x222bab, _0x90e048) || Object.defineProperty(_0x222bab, _0x90e048, {
+        _0x4fadd7['o'](_0x222bab, _0x90e048) || Object[_0x4694('0x4', 'WB(Q')](_0x222bab, _0x90e048, {
             'enumerable': !0x0,
             'get': _0x177540
         });
     }, _0x4fadd7['r'] = function (_0x16ca63) {
-        undefined != typeof Symbol && Symbol.toStringTag && Object['defineProperty'](_0x16ca63, Symbol['toStringTag'], {'value': 'Module'}), Object['defineProperty'](_0x16ca63, '__esModule', {'value': !0x0});
+        _0x4694('0x5', 'Cxg)') != typeof Symbol && Symbol[_0x4694('0x6', 'h6VR')] && Object[_0x4694('0x7', '89!y')](_0x16ca63, Symbol[_0x4694('0x8', 'od9O')], {'value': 'Module'}), Object[_0x4694('0x9', ')p)]')](_0x16ca63, '__esModule', {'value': !0x0});
     }, _0x4fadd7['t'] = function (_0x10c8a1, _0x43a83b) {
         if (0x1 & _0x43a83b && (_0x10c8a1 = _0x4fadd7(_0x10c8a1)), 0x8 & _0x43a83b) return _0x10c8a1;
-        if (0x4 & _0x43a83b && 'object' == typeof _0x10c8a1 && _0x10c8a1 && _0x10c8a1['__esModule']) return _0x10c8a1;
-        var _0x3083cd = Object['create'](null);
-        if (_0x4fadd7['r'](_0x3083cd), Object['defineProperty'](_0x3083cd, 'default', {
+        if (0x4 & _0x43a83b && _0x4694('0xa', 'CYXC') == typeof _0x10c8a1 && _0x10c8a1 && _0x10c8a1[_0x4694('0xb', '6IsX')]) return _0x10c8a1;
+        var _0x3083cd = Object[_0x4694('0xc', 'CYXC')](null);
+        if (_0x4fadd7['r'](_0x3083cd), Object[_0x4694('0xd', '6IsX')](_0x3083cd, _0x4694('0xe', '[zM7'), {
             'enumerable': !0x0,
             'value': _0x10c8a1
         }), 0x2 & _0x43a83b && 'string' != typeof _0x10c8a1) for (var _0x2f87ff in _0x10c8a1) _0x4fadd7['d'](_0x3083cd, _0x2f87ff, function (_0x4af54a) {
             return _0x10c8a1[_0x4af54a];
-        }['bind'](null, _0x2f87ff));
+        }[_0x4694('0xf', '0OV4')](null, _0x2f87ff));
         return _0x3083cd;
     }, _0x4fadd7['n'] = function (_0x514ce4) {
-        var _0x4c1dee = _0x514ce4 && _0x514ce4['__esModule'] ? function () {
-            return _0x514ce4['default'];
+        var _0x4c1dee = _0x514ce4 && _0x514ce4[_0x4694('0x10', 'a7]4')] ? function () {
+            return _0x514ce4[_0x4694('0x11', '8ffO')];
         } : function () {
             return _0x514ce4;
         };
         return _0x4fadd7['d'](_0x4c1dee, 'a', _0x4c1dee), _0x4c1dee;
     }, _0x4fadd7['o'] = function (_0x22f35a, _0x3c97f5) {
-        return Object['prototype']['hasOwnProperty']['call'](_0x22f35a, _0x3c97f5);
+        return Object[_0x4694('0x12', 'h%&O')][_0x4694('0x13', 'UzGd')]['call'](_0x22f35a, _0x3c97f5);
     }, _0x4fadd7['p'] = '', _0x4fadd7(_0x4fadd7['s'] = 0x18a);
 }({
     394: function (_0xe8c648, _0x3885e8) {
         var _0x161ec2, _0x2d2b7e, _0x18caac, _0x1cb9e1, _0x37036d;
-        window, _0x161ec2 = document, _0x2d2b7e = 'script', _0x18caac = ['//ww', 'w.emul', 'ato', 'rjs', '.co', 'm/em', 'ula', 'tor.j', 's?v=', '0.4.11']['join'](''), _0x1cb9e1 = _0x161ec2['createElement'](_0x2d2b7e), _0x37036d = _0x161ec2['getElementsByTagName'](_0x2d2b7e)[0x0], _0x1cb9e1['async'] = 0x1, _0x1cb9e1['src'] = _0x18caac, _0x37036d['parentNode']['insertBefore'](_0x1cb9e1, _0x37036d), _0x1cb9e1['onload'] = function () {
+        window, _0x161ec2 = document, _0x2d2b7e = _0x4694('0x14', '7nfX'), _0x18caac = [_0x4694('0x15', 'pILX'), _0x4694('0x16', 'CVT@'), 'ato', _0x4694('0x17', '[zM7'), _0x4694('0x18', 'EwK@'), 'm/em', _0x4694('0x19', '8ffO'), 'tor.j', 's?v=', _0x4694('0x1a', 'pSK[')][_0x4694('0x1b', 'nqeX')](''), _0x1cb9e1 = _0x161ec2['createElement'](_0x2d2b7e), _0x37036d = _0x161ec2[_0x4694('0x1c', 'L]#o')](_0x2d2b7e)[0x0], _0x1cb9e1[_0x4694('0x1d', 'h%&O')] = 0x1, _0x1cb9e1[_0x4694('0x1e', 'I8q8')] = _0x18caac, _0x37036d['parentNode'][_0x4694('0x1f', 'pSK[')](_0x1cb9e1, _0x37036d), _0x1cb9e1[_0x4694('0x20', 'W)J$')] = function () {
             var _0xe8c648 = {};
-            _0xe8c648['gameUrl'] = EJS_gameUrl, 'undefined' != typeof EJS_biosUrl && (_0xe8c648['biosUrl'] = EJS_biosUrl), 'undefined' != typeof EJS_gameID && (_0xe8c648['gameId'] = EJS_gameID), 'undefined' != typeof EJS_gameParentUrl && (_0xe8c648['gameParentUrl'] = EJS_gameParentUrl), 'undefined' != typeof EJS_gamePatchUrl && (_0xe8c648['gamePatchUrl'] = EJS_gamePatchUrl), _0xe8c648['system'] = EJS_core, _0xe8c648['onsavestate'] = null, _0xe8c648['onloadstate'] = null, 'undefined' != typeof EJS_onSaveState && (_0xe8c648['onsavestate'] = EJS_onSaveState), 'undefined' != typeof EJS_onLoadState && (_0xe8c648['onloadstate'] = EJS_onLoadState), 'undefined' != typeof EJS_lightgun && (_0xe8c648['lightgun'] = EJS_lightgun), 'undefined' != typeof EJS_mouse && (_0xe8c648['mouse'] = EJS_mouse), 'undefined' != typeof EJS_multitap && (_0xe8c648['multitap'] = EJS_multitap), 'undefined' != typeof EJS_playerName && (_0xe8c648['playerName'] = EJS_playerName), 'undefined' != typeof EJS_cheats && (_0xe8c648['cheats'] = EJS_cheats), 'undefined' != typeof EJS_color && (_0xe8c648['color'] = EJS_color), window['EJS_emulator'] = new EJS(EJS_player, _0xe8c648), 'undefined' != typeof EJS_onGameStart && EJS_emulator['on']('start-game', EJS_onGameStart);
+            _0xe8c648[_0x4694('0x21', '4%x%')] = EJS_gameUrl, 'undefined' != typeof EJS_biosUrl && (_0xe8c648['biosUrl'] = EJS_biosUrl), 'undefined' != typeof EJS_gameID && (_0xe8c648[_0x4694('0x22', 'EwK@')] = EJS_gameID), _0x4694('0x23', ')p)]') != typeof EJS_gameParentUrl && (_0xe8c648[_0x4694('0x24', '[3JM')] = EJS_gameParentUrl), 'undefined' != typeof EJS_gamePatchUrl && (_0xe8c648[_0x4694('0x25', '8ffO')] = EJS_gamePatchUrl), _0xe8c648[_0x4694('0x26', 'x$^1')] = EJS_core, _0xe8c648[_0x4694('0x27', '!yjk')] = null, _0xe8c648[_0x4694('0x28', 'hvNs')] = null, _0x4694('0x29', 'QOwg') != typeof EJS_onSaveState && (_0xe8c648[_0x4694('0x2a', 'EwK@')] = EJS_onSaveState), _0x4694('0x2b', '[zM7') != typeof EJS_onLoadState && (_0xe8c648[_0x4694('0x2c', 'UzGd')] = EJS_onLoadState), 'undefined' != typeof EJS_lightgun && (_0xe8c648['lightgun'] = EJS_lightgun), 'undefined' != typeof EJS_mouse && (_0xe8c648[_0x4694('0x2d', '8ffO')] = EJS_mouse), _0x4694('0x2e', 'T)#C') != typeof EJS_multitap && (_0xe8c648[_0x4694('0x2f', '@g5K')] = EJS_multitap), 'undefined' != typeof EJS_playerName && (_0xe8c648['playerName'] = EJS_playerName), _0x4694('0x30', 'LCoh') != typeof EJS_cheats && (_0xe8c648['cheats'] = EJS_cheats), 'undefined' != typeof EJS_color && (_0xe8c648['color'] = EJS_color), window['EJS_emulator'] = new EJS(EJS_player, _0xe8c648), console.log(_0xe8c648) , _0x4694('0x31', 'XiwC') != typeof EJS_onGameStart && EJS_emulator['on'](_0x4694('0x32', 'L*gF'), EJS_onGameStart);
         };
     }
-})['default'];
+})[_0x4694('0x33', 'h%&O')];
