2dc0242579b6aef3b66af69feb2c7a9e1eb053f7
fansheng <185085781@qq.com>
2021-12-22 03:10:48 +0800

    Default Changelist

--- fortress-action.js
+++ fortress-action.js
@@ -8,18 +8,6 @@
     if(window.fortress){
         window.fortress(path);
     }
-    function getUrlData(url){
-        var data = null;
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange=function(){
-            if(xmlhttp.readyState===4 && xmlhttp.status===200){
-                data = xmlhttp.responseText;
-            }
-        }
-        xmlhttp.open("GET",url,false);
-        xmlhttp.send();
-        return data;
-    }
     function currentPath(){
         var src = document.currentScript.src;
         var ss = src.split("/");
