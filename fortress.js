c97b7d2dd33fcf916db6fabe9a485df5a3440184
fansheng <185085781@qq.com>
2021-12-22 03:35:10 +0800

    Default Changelist

--- fortress.js
+++ fortress.js
@@ -1,13 +1,24 @@
 (function (){
-    /*堡垒机,用于识别使用哪个cdn的,以后所有的情况改这里就行,由七牛云负责主加载*/
-    var tmpSz = [
-        "https://fs185085781.gitee.io/cdn",
-        "https://objective-kepler-55a6d4.netlify.app",
-        "https://cdn.jsdelivr.net/gh/fs185085781/cdn@v1.1.26",
-        currentPath()
-    ];
-    for(var i=0;i<tmpSz.length;i++){
-        document.write('<script src="'+tmpSz[i]+'/fortress-action.js"></script>');
+    /*堡垒机,用于识别使用哪个cdn的,以后所有的情况改这里就行,由gitee pages负责主加载*/
+    if(!window.fortressAction){
+        window.fortressAction = 1;
+        var tmpSz = [
+            "https://cdn.jsdelivr.net/gh/fs185085781/cdn@v1.1.27",
+            "https://fs185085781.gitee.io/cdn",
+            "https://objective-kepler-55a6d4.netlify.app"
+        ];
+        for(var i=0;i<tmpSz.length;i++){
+            document.write('<script src="'+tmpSz[i]+'/fortress.js"></script>');
+        }
+        return;
+    }
+    if(window.fortressAction == 2){
+        return;
+    }
+    window.fortressAction = 2;
+    var path = currentPath();
+    if(window.fortress){
+        window.fortress(path);
     }
     function currentPath(){
         var src = document.currentScript.src;
