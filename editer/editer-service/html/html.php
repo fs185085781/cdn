2c4dc66bdb4a0457ddc3979b601d37d000adaaa9
fansheng <185085781@qq.com>
2021-12-27 15:25:33 +0800

    1

--- editer/editer-service/html/html.php
+++ editer/editer-service/html/html.php
@@ -1,8 +1,12 @@
 <?php
-$cdn = json_decode(file_get_contents("../cdn.js"),true);
-$editerPath = $cdn['cdn']."/editer/";
+function getCdn(){
+    $str = file_get_contents("../cdn.js");
+    $str = strstr($str,"http");
+    $str = strstr($str,'"',true);
+    return $str;
+}
+$editerPath = getCdn()."/editer/";
 $toPathName = $_GET['toPathName'];
 $html = file_get_contents($editerPath.$toPathName);
 $html = str_replace('src="asserts','src="'.$editerPath.'asserts',$html);
 echo $html;
-
